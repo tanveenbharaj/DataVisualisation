b92baed5e519ee4a8297bd651dca60cd54890653
tanveen <tanveen.bharaj@utah.edu>
2017-09-13 21:51:54 -0600

    d3 transitions

--- hw3/script.js
+++ hw3/script.js
@@ -5,6 +5,17 @@
  */
 function staircase() {
     // ****** TODO: PART II ******
+    var selection = document.getElementById("first_bar_chart");
+    var childNodes = selection.childNodes;
+    var i = 0;
+    var j = 0;
+    for (i = 1; i < childNodes.length; i += 2) {
+        if (childNodes[i] != null) {
+            childNodes[i].setAttribute('height', j * 10);
+            j++;
+        }
+    }
+
 }
 
 /**
@@ -41,40 +52,218 @@ function update(error, data) {
     // Set up the scales
     let aScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.a)])
-        .range([0, 150]);
+.range([0, 150]);
     let bScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.b)])
-        .range([0, 150]);
+.range([0, 150]);
     let iScale = d3.scaleLinear()
         .domain([0, data.length])
         .range([0, 110]);
-
 
     // ****** TODO: PART III (you will also edit in PART V) ******
 
     // TODO: Select and update the 'a' bar chart bars
 
+    var svg = d3.select("#first_bar_chart");
+
+    var bars = svg.selectAll("rect")
+        .data(data);
+
+    bars.exit().attr("opacity", 1)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 0)
+        .remove();
+
+    bars = bars.enter().append("rect").classed("barChart", true)
+    // merge the enter and the update selection
+        .merge(bars);
+
+    bars.transition()
+        .duration(3000)
+        .attr("y", 0)
+        .attr("x", function(d, i) {
+            return iScale(i);
+        })
+        .attr("height", function(d) {
+            return aScale(d.a);
+        })
+        .attr("width", 10)
+        .style("fill", "steelblue");
+
+    bars.on("mouseover", function() {
+        d3.select(this).style("fill", "red");
+    }).on("mouseout", function() {
+        d3.select(this)
+            .style("fill", "steelblue");
+    });
+
     // TODO: Select and update the 'b' bar chart bars
+
+    var svg = d3.select("#second_bar_chart");
+
+    var bars = svg.selectAll("rect")
+        .data(data);
+
+    bars.exit().attr("opacity", 1)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 0)
+        .remove();
+
+    bars = bars.enter().append("rect").classed("barChart", true)
+        .merge(bars);
+
+    bars.transition()
+        .duration(3000)
+        .attr("y", 0)
+        .attr("x", function(d, i) {
+            return iScale(i);
+        })
+        .attr("height", function(d) {
+            return bScale(d.b);
+        })
+        .attr("width", 10)
+        .style("fill", "steelblue")
+        .attr("opacity", 1);
+    bars.on("mouseover", function() {
+        d3.select(this).style("fill", "red");
+    }).on("mouseout", function() {
+        d3.select(this)
+            .style("fill", "steelblue");
+    });
 
     // TODO: Select and update the 'a' line chart path using this line generator
 
     let aLineGenerator = d3.line()
         .x((d, i) => iScale(i))
-        .y((d) => aScale(d.a));
+.y((d) => aScale(d.a));
+
+    svg = d3.select("#first_line_chart");
+    var a_line = svg.select("path")
+        .data(data);
+
+    a_line.exit().attr("opacity", 1)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 0).remove();
+
+    var newLine = a_line.enter().append("path");
+    a_line = a_line.merge(newLine);
+
+    a_line.transition()
+        .duration(3000)
+        .attr('d', aLineGenerator(data))
+        .attr("class", "lines");
 
     // TODO: Select and update the 'b' line chart path (create your own generator)
+
+    let bLineGenerator = d3.line()
+        .x((d, i) => iScale(i))
+.y((d) => aScale(d.b));
+
+    svg = d3.select("#second_line_chart");
+    var b_line = svg.select("path")
+        .data(data);
+
+    b_line.exit().attr("opacity", 1)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 0).remove();
+
+    b_line = b_line.enter().append("path").merge(b_line);
+
+    b_line.transition()
+        .duration(3000).attr('d', bLineGenerator(data)).attr("class", "lines");
 
     // TODO: Select and update the 'a' area chart path using this area generator
     let aAreaGenerator = d3.area()
         .x((d, i) => iScale(i))
-        .y0(0)
+.y0(0)
         .y1(d => aScale(d.a));
+
+    svg = d3.select("#first_area_chart");
+    var a_path = svg.selectAll("path")
+        .data(data);
+
+    a_path.exit().attr("opacity", 1)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 0).remove();
+
+    a_path = a_path.enter().append("path").merge(a_path);
+
+    a_path.transition()
+        .duration(3000).attr('d', aAreaGenerator(data)).attr("class", "areas");
 
     // TODO: Select and update the 'b' area chart path (create your own generator)
 
+    let bAreaGenerator = d3.area()
+        .x((d, i) => iScale(i))
+.y0(0)
+        .y1(d => aScale(d.b));
+
+    svg = d3.select("#second_area_chart");
+    var a_path = svg.selectAll("path")
+        .data(data);
+
+    a_path.exit().attr("opacity", 1)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 0).remove();
+
+    a_path = a_path.enter().append("path").merge(a_path);
+
+    a_path.transition()
+        .duration(3000).attr('d', bAreaGenerator(data)).attr("class", "areas");
+
     // TODO: Select and update the scatterplot points
 
+    svg = d3.select("#scatter_plot");
+    var circles = svg.selectAll("circle")
+        .data(data);
+    circles.exit().attr("opacity", 1)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 0).remove();
+    var newData = circles.enter().append("circle");
+    circles = newData.merge(circles);
+    circles.transition()
+        .duration(3000)
+        .attr("cx", function(d) {
+            return d.a * 10;
+        })
+        .attr("cy", function(d) {
+            return d.b * 10;
+        })
+        .attr("r", function(d) {
+            return 5;
+        });
+    circles.classed("frame");
+    circles.on("click", function(d) {
+        console.log('The x & y co-ordinates of selected point is : ' + d.a + ',' + d.b)
+    });
+
     // ****** TODO: PART IV ******
+
+    //adding tooltip
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    circles.on("mouseover", function(d) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html('(' + d.a + ',' + d.b + ')')
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY) + "px");
+    })
+        .on("mouseout", function(d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
 }
 
@@ -85,8 +274,7 @@ function changeData() {
     let dataFile = document.getElementById('dataset').value;
     if (document.getElementById('random').checked) {
         randomSubset();
-    }
-    else {
+    } else {
         d3.csv('data/' + dataFile + '.csv', update);
     }
 }
@@ -97,7 +285,7 @@ function changeData() {
 function randomSubset() {
     let dataFile = document.getElementById('dataset').value;
     if (document.getElementById('random').checked) {
-        d3.csv('data/' + dataFile + '.csv', function (error, data) {
+        d3.csv('data/' + dataFile + '.csv', function(error, data) {
             let subset = [];
             for (let d of data) {
                 if (Math.random() > 0.5) {
@@ -106,8 +294,7 @@ function randomSubset() {
             }
             update(error, subset);
         });
-    }
-    else {
+    } else {
         changeData();
     }
 }
